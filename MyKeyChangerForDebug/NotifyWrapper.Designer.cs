67a9b0a5d091fe78cfa3ec81cf2b7b826bd3f7bc
OSN-DEV <osndev1973@gmail.com>
2018-11-05 23:00:25 +0900

    コンテキストメニューを動的に作成するように修正

--- MyKeyChangerForDebug/NotifyWrapper.Designer.cs
+++ MyKeyChangerForDebug/NotifyWrapper.Designer.cs
@@ -28,10 +28,7 @@
             this.cNotify = new System.Windows.Forms.NotifyIcon(this.components);
             this.cMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.cWatch = new System.Windows.Forms.ToolStripMenuItem();
-            this.cMenu_Watch = new System.Windows.Forms.ToolStripMenuItem();
-            this.cMenuSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.cMenuExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.cMenuMode = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.cMenu.SuspendLayout();
             // 
             // cNotify
@@ -44,12 +41,9 @@
             // cMenu
             // 
             this.cMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.cMenu_Watch,
-            this.cMenuMode,
-            this.cMenuSeparator1,
-            this.cMenuExit});
+            this.toolStripMenuItem1});
             this.cMenu.Name = "cMenu";
-            this.cMenu.Size = new System.Drawing.Size(182, 81);
+            this.cMenu.Size = new System.Drawing.Size(61, 4);
             // 
             // cWatch
             // 
@@ -59,31 +53,13 @@
             this.cWatch.Size = new System.Drawing.Size(108, 22);
             this.cWatch.Text = "Watch";
             // 
-            // cMenu_Watch
+            // toolStripMenuItem1
             // 
-            this.cMenu_Watch.Name = "cMenu_Watch";
-            this.cMenu_Watch.Size = new System.Drawing.Size(181, 22);
-            this.cMenu_Watch.Text = "toolStripMenuItem1";
-            // 
-            // cMenuSeparator1
-            // 
-            this.cMenuSeparator1.Name = "cMenuSeparator1";
-            this.cMenuSeparator1.Size = new System.Drawing.Size(178, 6);
-            // 
-            // cMenuExit
-            // 
-            this.cMenuExit.Name = "cMenuExit";
-            this.cMenuExit.Size = new System.Drawing.Size(181, 22);
-            this.cMenuExit.Text = "toolStripMenuItem1";
-            // 
-            // cMenuMode
-            // 
-            this.cMenuMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cMenuMode.Items.AddRange(new object[] {
-            "Android",
-            "Visual Studio"});
-            this.cMenuMode.Name = "cMenuMode";
-            this.cMenuMode.Size = new System.Drawing.Size(121, 23);
+            this.toolStripMenuItem1.Checked = true;
+            this.toolStripMenuItem1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(32, 19);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
             this.cMenu.ResumeLayout(false);
 
         }
@@ -92,9 +68,6 @@
         private System.Windows.Forms.ContextMenuStrip cMenu;
         internal System.Windows.Forms.NotifyIcon cNotify;
         private System.Windows.Forms.ToolStripMenuItem cWatch;
-        private System.Windows.Forms.ToolStripMenuItem cMenu_Watch;
-        private System.Windows.Forms.ToolStripComboBox cMenuMode;
-        private System.Windows.Forms.ToolStripSeparator cMenuSeparator1;
-        private System.Windows.Forms.ToolStripMenuItem cMenuExit;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
     }
 }
--- MyKeyChangerForDebug/NotifyWrapper.cs
+++ MyKeyChangerForDebug/NotifyWrapper.cs
@@ -5,19 +5,62 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 
 namespace MyKeyChangerForDebug {
     public partial class NotifyWrapper : Component {
+
+        #region Constructor
         public NotifyWrapper() {
             InitializeComponent();
+            this.Initialize();
         }
 
         public NotifyWrapper(IContainer container) {
             container.Add(this);
-
             InitializeComponent();
+            this.Initialize();
         }
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// initialize this component
+        /// </summary>
+        private void Initialize() {
+            this.cMenu.SuspendLayout();
+            // create context menu by code, bacuase I can't find the way to create nested menu. 
+            this.cMenu.Items.Clear();
+
+            ToolStripMenuItem itemObserve = new ToolStripMenuItem();
+            itemObserve.Text = "observe";
+            itemObserve.ToolTipText = "observe to input ten key";
+            this.cMenu.Items.Add(itemObserve);
+
+            ToolStripMenuItem itemMode = new ToolStripMenuItem();
+            itemMode.Text = "mode";
+            this.cMenu.Items.Add(itemMode);
+
+            ToolStripMenuItem itemModeAndroid = new ToolStripMenuItem();
+            itemModeAndroid.Text = "Android";
+            itemModeAndroid.ToolTipText = "Key Assign is for Android";
+            itemMode.DropDown.Items.Add(itemModeAndroid);
+
+            ToolStripMenuItem itemModeDotNet = new ToolStripMenuItem();
+            itemModeDotNet.Text = ".Net";
+            itemModeDotNet.ToolTipText = "Key Assign is for .Net";
+            itemMode.DropDown.Items.Add(itemModeDotNet);
+
+            ToolStripSeparator separator = new ToolStripSeparator();
+            this.cMenu.Items.Add(separator);
+
+            ToolStripMenuItem itemExit = new ToolStripMenuItem();
+            itemExit.Text = "Exit";
+            itemExit.ToolTipText = "Exit Application";
+            this.cMenu.Items.Add(itemExit);
+            this.cMenu.ResumeLayout();
+        }
+        #endregion
 
 
         // notifyIcon1.Icon = Icon.FromHandle(((Bitmap)imageList1.Images[0]).GetHicon());
